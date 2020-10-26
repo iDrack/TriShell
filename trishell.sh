a3ff8a1e902f8a2cdc0facf532c3d19d5359493d
iDrack <tdignoire@protonmail.com>
2020-10-26 18:23:20 +0100

    Update trishell.sh (Ajout de l'option -d)

--- trishell.sh
+++ trishell.sh
@@ -1,26 +1,28 @@
 #!/bin/bash
 
-#list nous permettra de faire nos operations de tri
-list=''
-
-#Fonctions permettant d'operer sur notre liste
 function ajout(){
+    #Fonctions permettant d'operer sur notre liste
     list=$list' '$1
 }
 
 function taille_de(){
+    #Fonction retournant la taille de la liste passee en parametre
     return $#
 }
 
 function afficher(){
-    for e in $@
+    #Fonction permettant d'afficher la liste passee en parametre
+    echo -e ""
+    echo `pwd`:
+    for i in $@
         do
-        echo $e
+        echo -ne "$i\t"
         done
+    echo ""
 }
 
 function suppression(){
-    #Premier parametre = elem a supp, second parametre =list
+    #Premier parametre = elem a supp, second parametre = list a manipuler
     tmp=''
     for e in $@
         do
@@ -29,15 +31,34 @@ function suppression(){
     list=$tmp
 }
 
-if [ "$1" = "-R" ]
-then
-    echo ".:"
-fi
-for i in *
-    do
-    echo -ne "$i\t"
-done
-echo ""
+function inverser_ordre(){
+    #Permet d'inverser la liste d'elements
+    tmp=''
+    for e in $@
+        do
+        tmp=$e' '$tmp
+        done
+    list=$tmp
+}
+
+function new_list(){
+    #Permet de creer une nouvelle liste et de gerer les options
+    #Il faudra passer en parametre les options passer lors de l'appel de la commande
+    list=*
+    for e in $@
+        do
+        if [ "$e" = "-d" ]
+            then
+            inverser_ordre $list
+        fi
+    done
+}
+
+#Ajout des elements a afficher dans la liste
+new_list $@
+
+#Voici la fonction principal
+afficher $list
 if [ "$1" = "-R" ]
 then
     for i in *
@@ -45,12 +66,8 @@ then
         if [ -d "$i" ]
         then
             cd $i
-	    echo -e "\n./$i:"
-            for i in *
-            do
-                echo -ne "$i\t"
-            done
-	    echo ""
+            new_list $@ #On appel new_list afin de recuperer les fichiers du nouveau repertoie
+            afficher $list
             cd ..
         fi
     done
